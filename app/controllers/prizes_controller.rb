7988ee0ffd6c48be51e2473d89913be87be845cd
Heath Attig <heath.attig@gmail.com>
2013-09-12 04:51:45 -0500

    prize controller specs

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -1,10 +1,8 @@
 class PrizesController < ApplicationController
   def new
     @category = Category.find(params[:category_id])
-    ### not tested
     @prize = @category.prizes.build
     @prize.prize_elements.build
-    ### not tested
   end
 
   def index
@@ -15,11 +13,9 @@ class PrizesController < ApplicationController
 
   def create
     @category = Category.find(params[:category_id])
-    ### not tested
     @prize = @category.prizes.build(params[:prize])
     if @prize.save
       redirect_to category_path(@category)
     end
-    ### not tested
   end
 end
--- spec/controllers/prizes_controller_spec.rb
+++ spec/controllers/prizes_controller_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+require 'pry'
 
 describe PrizesController do
   let(:category_attributes) { FactoryGirl.attributes_for(:category) }
+  let(:prize_attributes) { FactoryGirl.attributes_for(:prize)}
+  let(:prize_element_attributes) {FactoryGirl.attributes_for(:prize_element)}
 
   describe "GET prize/new" do
     it "assigns category from url as @category" do
@@ -10,22 +13,51 @@ describe PrizesController do
       assigns(:category).should eq(category)
     end
 
-    it "assigns a new prize to category"
-    it "builds elements for prize"
+    it "assigns a new prize to category" do
+      category = Category.create! category_attributes
+      get :new, {category_id: category.to_param}
+      assigns(:prize).should be_a_new(Prize)
+      assigns(:prize).should_not be_persisted
+    end
+
+    it "builds elements for prize" do
+      category = Category.create! category_attributes
+      get :new, {category_id: category.to_param}
+      assigns(:prize).prize_elements.should_not be_empty
+    end
   end
 
   describe "POST prize/new" do
     context "with valid data" do
 
+
+      it "creates a new prize" do
+        category = Category.create! category_attributes
+        expect do
+          post :create, {
+            category_id: category.to_param, 
+            prize: FactoryGirl.attributes_for(:prize_with_elements)}
+        end
+        .to change(Prize, :count).by(1)
+      end
+
       it "assigns category from url as @category" do
         category = Category.create! category_attributes
-        post :new, {category_id: category.to_param}
+        post :create, {category_id: category.to_param}
         assigns(:category).should eq(category)
       end
 
-      it
+      # it "assigns newly created prize as @prize" do
+      #   category = Category.create! category_attributes
+      #   post :new, {category_id: category.to_param}
+      #   assigns(:prize).should 
+
+      # end
 
       it "redirect to category index" do
+        category = Category.create! category_attributes
+        post :create, {category_id: category.to_param}
+        response.should redirect_to(category_path(category))
       end
     end
   end
